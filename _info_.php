b1210863666ef66490c2b4895b8d8d11e5b9f80d
xtr4nge <xtr4nge@gmail.com>
2013-12-09 23:00:36 +0000

    minor fix

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="ngrep";
-$mod_version="1.1";
+$mod_version="1.2";
 $mod_logs="/usr/share/FruityWifi/logs/ngrep.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/ngrep/includes/logs/";
 $mod_path="/usr/share/FruityWifi/www/modules/ngrep";
